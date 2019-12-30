9f4eddb3d56173ad5f80e8498a923d7ce0f491e0
USER <USER@USER-PC>
2019-12-30 16:29:51 +0530

    Day4-Use of final keyword

---
+++ github.com/VIGNESH-MANIKANDAN/MYPROJECTS.git/Day4_JFS_FINALKEYWORD/src/com/cvmanik/FinalKeyword/Main.java
@@ -0,0 +1,17 @@
+package com.cvmanik.FinalKeyword;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int number;
+		Scanner sc=new Scanner(System.in);
+		number=sc.nextInt();  // cannot be modified because it is made as variable.
+		System.out.println(number);
+
+	}
+
+}
